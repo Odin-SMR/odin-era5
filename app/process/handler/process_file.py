1fcf1cde4089ab02a3f7833a299efb2dd690ec74
Joakim Möller <joakim.moller@molflow.com>
2023-06-27 23:16:49 +0200

    Better parameter handling, some tooling... (#3)
    * better parameter handling, some tooling...
    
    * lint fixes
    
    * fix after review comments

--- app/process/handler/process_file.py
+++ app/process/handler/process_file.py
@@ -3,6 +3,16 @@ import json
 
 import boto3
 
+import hashlib
+import time
+
+
+def create_short_hash():
+    input_data = str(time.time()).encode("utf-8")
+    hash_object = hashlib.sha256(input_data)
+    short_hash = hash_object.hexdigest()[:8]
+    return short_hash
+
 
 def find_arn():
     client = boto3.client("stepfunctions")
@@ -14,13 +24,12 @@ def find_arn():
 
 
 def lambda_handler(event, context):
-    # Calculate the date of five days ago
     current_date = datetime.date.today()
     six_days_ago = current_date - datetime.timedelta(days=6)
+    date = event.get("date", six_days_ago.isoformat())
 
     state_machine_arn = find_arn()
 
-    # Create four different times
     hours = [
         "00",
         "06",
@@ -31,11 +40,12 @@ def lambda_handler(event, context):
     sfn = boto3.client("stepfunctions")
 
     for hour in hours:
-        input_data = {"date": six_days_ago.isoformat(), "hour": hour}
+        input_data = {"date": date, "hour": hour}
 
-        response = sfn.start_execution(
+        sfn.start_execution(
             stateMachineArn=state_machine_arn,
             input=json.dumps(input_data),
+            name=f"{date}T{hour}-{create_short_hash()}",
         )
 
     return {
--- scripts/start_state_machines.py
+++ scripts/start_state_machines.py
@@ -1,41 +1,45 @@
 #!/bin/env python3
-import datetime
+import argparse
 import json
+import sys
+from datetime import date, timedelta
 
-import boto3
+from git.repo import Repo
 
 
-def find_arn() -> str:
-    client = boto3.client("stepfunctions")
-    results = client.list_state_machines()
-    for i in results["stateMachines"]:
-        if i["name"] == "Era5StateMachine":
-            return i["stateMachineArn"]
-    return "None"
+def get_git_root() -> str:
+    repo = Repo(".", search_parent_directories=True)
+    return repo.git.rev_parse("--show-toplevel")
+
+
+sys.path.insert(0, get_git_root())
+
+
+from app.process.handler.process_file import lambda_handler
+
+
+def valid_date(iso_formatted: str) -> date:
+    try:
+        valid_date = date.fromisoformat(iso_formatted)
+    except ValueError:
+        raise argparse.ArgumentTypeError("date must be iso-formatted (YYYY-MM-DD)")
+    return valid_date
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Start ERA5 pipeline")
+    parser.add_argument(
+        "-s", "--start", help="Start date YYYY-MM-DD", type=valid_date, required=True
+    )
+    parser.add_argument(
+        "-e", "--end", help="End date YYYY-MM-DD", type=valid_date, required=True
+    )
+    args = parser.parse_args()
+    current_date: date = args.start
+    while current_date <= args.end:
+        lambda_handler({"date": current_date}, None)
+        current_date = current_date + timedelta(days=1)
 
 
 if __name__ == "__main__":
-    current_date = datetime.date(2023, 1, 1)
-    end_date = datetime.date.today() - datetime.timedelta(days=4)
-
-    state_machine_arn = find_arn()
-
-    # Create four different times
-    hours = [
-        "00",
-        "06",
-        "12",
-        "18",
-    ]
-
-    sfn = boto3.client("stepfunctions")
-    while current_date <= end_date:
-        for hour in hours:
-            input_data = json.dumps({"date": current_date.isoformat(), "hour": hour})
-
-            response = sfn.start_execution(
-                stateMachineArn=state_machine_arn,
-                input=input_data,
-                name=f"{current_date}T{hour}-V3",
-            )
-        current_date = current_date + datetime.timedelta(days=1)
+    main()
